0cacc696781ec65d938aca08c077783ad30c6fe0
AngelForester <156679206+AngelForester@users.noreply.github.com>
2025-02-28 09:08:32 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -24,7 +24,7 @@ app.config(function ($routeProvider, $locationProvider) {
         controller: "asistenciasCtrl"
     })
     .when("/empleados", {
-        templateUrl: "/empleado",
+        templateUrl: "/empleados",
         controller: "empleadoCtrl"
     })
     .when("/reportes", {

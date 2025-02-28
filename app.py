c94669f2c898274d4774671a6500c8056a262077
AngelForester <156679206+AngelForester@users.noreply.github.com>
2025-02-28 02:04:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -45,20 +45,37 @@ def app2():
 
     return "<h5>Hola, soy la view app</h5>"
 
-@app.route("/productos")
+@app.route("/asistencias")
 def productos():
     if not con.is_connected():
         con.reconnect()
 
     cursor = con.cursor(dictionary=True)
     sql    = """
-    SELECT Id_Producto,
-           Nombre_Producto,
-           Precio,
-           Existencias
+    SELECT 	`idAsistencia`,
+    		`empleado`,
+            `reporte`,
+            `reporte`
+    FROM `vistatotal`
+    LIMIT 10 OFFSET 0
+    """
 
-    FROM productos
+    cursor.execute(sql)
+    registros = cursor.fetchall()
 
+    return render_template("asistencias.html", productos=registros)
+@app.route("/empleados")
+def productos():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT `idEmpleado`, 
+            `nombreEmpleado`, 
+            `numero`, 
+            `fechaIngreso` 
+    FROM empleados 
     LIMIT 10 OFFSET 0
     """
 
@@ -68,142 +85,36 @@ def productos():
     # Si manejas fechas y horas
     """
     for registro in registros:
-        fecha_hora = registro["Fecha_Hora"]
+        fecha_hora = registro["fechaIngreso"]
 
         registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
-        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
-        registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
     """
 
-    return render_template("productos.html", productos=registros)
+    return render_template("empleados.html", productos=registros)
 
-@app.route("/productos/buscar", methods=["GET"])
-def buscarProductos():
+@app.route("/reportes")
+def productos():
     if not con.is_connected():
         con.reconnect()
 
-    args     = request.args
-    busqueda = args["busqueda"]
-    busqueda = f"%{busqueda}%"
-    
     cursor = con.cursor(dictionary=True)
     sql    = """
-    SELECT Id_Producto,
-           Nombre_Producto,
-           Precio,
-           Existencias
-
-    FROM productos
-
-    WHERE Nombre_Producto LIKE %s
-    OR    Precio          LIKE %s
-    OR    Existencias     LIKE %s
-
-    ORDER BY Id_Producto DESC
-
+    SELECT `idReporte`,
+		    `fecha`,
+            `comentarios`
+    FROM `reportes`
     LIMIT 10 OFFSET 0
     """
-    val    = (busqueda, busqueda, busqueda)
 
-    try:
-        cursor.execute(sql, val)
-        registros = cursor.fetchall()
-
-        # Si manejas fechas y horas
-        """
-        for registro in registros:
-            fecha_hora = registro["Fecha_Hora"]
-
-            registro["Fecha_Hora"] = fecha_hora.strftime("%Y-%m-%d %H:%M:%S")
-            registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
-            registro["Hora"]       = fecha_hora.strftime("%H:%M:%S")
-        """
-
-    except mysql.connector.errors.ProgrammingError as error:
-        print(f"Ocurrió un error de programación en MySQL: {error}")
-        registros = []
-
-    finally:
-        con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/producto", methods=["POST"])
-# Usar cuando solo se quiera usar CORS en rutas específicas
-# @cross_origin()
-def guardarProducto():
-    if not con.is_connected():
-        con.reconnect()
-
-    id          = request.form["id"]
-    nombre      = request.form["nombre"]
-    precio      = request.form["precio"]
-    existencias = request.form["existencias"]
-    # fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
-    
-    cursor = con.cursor()
-
-    if id:
-        sql = """
-        UPDATE productos
-
-        SET Nombre_Producto = %s,
-            Precio          = %s,
-            Existencias     = %s
-
-        WHERE Id_Producto = %s
-        """
-        val = (nombre, precio, existencias, id)
-    else:
-        sql = """
-        INSERT INTO productos (Nombre_Producto, Precio, Existencias)
-                    VALUES    (%s,          %s,      %s)
-        """
-        val =                 (nombre, precio, existencias)
-    
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
-
-    return make_response(jsonify({}))
-
-@app.route("/producto/<int:id>")
-def editarProducto(id):
-    if not con.is_connected():
-        con.reconnect()
-
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    SELECT Id_Producto, Nombre_Producto, Precio, Existencias
-
-    FROM productos
-
-    WHERE Id_Producto = %s
-    """
-    val    = (id,)
-
-    cursor.execute(sql, val)
+    cursor.execute(sql)
     registros = cursor.fetchall()
-    con.close()
 
-    return make_response(jsonify(registros))
-
-@app.route("/producto/eliminar", methods=["POST"])
-def eliminarProducto():
-    if not con.is_connected():
-        con.reconnect()
-
-    id = request.form["id"]
-
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    DELETE FROM productos
-    WHERE Id_Producto = %s
+    # Si manejas fechas y horas
     """
-    val    = (id,)
+    for registro in registros:
+        fecha_hora = registro["fecha"]
 
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
+        registro["Fecha"]      = fecha_hora.strftime("%d/%m/%Y")
+    """
 
-    return make_response(jsonify({}))
+    return render_template("reportes.html", productos=registros)
